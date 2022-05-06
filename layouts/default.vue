ae137ac68004484a629d6eb9fc99cc3a267c625e
alonemazin <10653747+alonemazin@users.noreply.github.com>
2022-05-07 02:42:01 +0300

    init

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <header class="center w-full ">
+      <div id="map-wrap" class="relative" style="height: 20vh; z-index:-1">
+        <client-only>
+          <l-map :zoom=13 :center="[55.9464418,8.1277591]">
+            <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+            <l-marker :lat-lng="[55.9464418,8.1277591]"></l-marker>
+          </l-map>
+        </client-only>
+      </div>
+      <div class="pic flex justify-center" style="margin-top: -100px;z-index: 1">
+        <img src="https://avatars.githubusercontent.com/u/9112378?v=4" style="height:200px" class="rounded-full">
+      </div>
+    </header>
+
+    <Nuxt />
+
+    <footer class="">
+
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+
+  }
+}
+</script>
---
+++ pages/_username.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="text-center mt-10">
+    <section id="soical">
+      1
+    </section>
+    <section id="health">
+      2
+    </section>
+    <section id="bio">
+      3
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
